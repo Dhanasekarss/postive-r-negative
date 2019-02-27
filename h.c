75f9c8bade891520f75b12df8fb42050d8344df6
Dhanasekarss <48053021+Dhanasekarss@users.noreply.github.com>
2019-02-27 16:23:59 +0530

    Update h.c

--- h.c
+++ h.c
@@ -1,1 +1,16 @@
-hello
+public class Main
+{
+	public static void main(String[] args) {
+		int number = 3;
+
+        if (number < 0)
+            System.out.println(number + " is a negative number.");
+
+        else if ( number > 0)
+            System.out.println(number + " is a positive number.");
+
+        else
+            System.out.println(number + " is 0.");
+    
+	}
+}
